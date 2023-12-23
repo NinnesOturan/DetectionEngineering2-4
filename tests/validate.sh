8b921e533363228f2efb80c42c5a05a78e8b00a2
NinnessOtu <hamid.bouhnik.it@gmail.com>
2023-12-23 14:40:25 -0500

    Create tthe rules and validate sigma

---
+++ tests/validate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if the check-jsonschema tool is installed
+if ! command -v check-jsonschema &> /dev/null
+then
+    echo "check-jsonschema could not be found"
+    echo "Please install it from PyPI using:"
+    echo "pip install check-jsonschema"
+    exit
+fi
+
+# Validate all the Sigma rules in the current directory
+echo "Validating Sigma rules against sigma-schema.json"
+check-jsonschema --schemafile tests/sigma-schema.json $(find ./rules  -type f -name "*.yml")
