ad27131dca1e6f43f1b5c8acb289e41ca1a59251
NinnessOtu <154692418+NinnessOtu@users.noreply.github.com>
2023-12-25 12:45:57 +0100

    Update convert.sh

--- tests/convert.sh
+++ tests/convert.sh
@@ -12,4 +12,11 @@ fi
 # Validate all the Sigma rules in the current directory
 echo "Converting Sigma rules to Elastalert"
 #check-jsonschema --schemafile tests/sigma-schema.json $(find ./rules  -type f -name "*.yml")
-sigmac -t elastalert -c tests/winlogbeat.yml $(find ./rules  -type f -name "*.yml") --output ./ESRules
+#sigmac -t elastalert-dsl -c tests/winlogbeat.yml -r $(find ./rules  -type f -name "*.yml") --output $(find ./ESRules)
+for file in rules/*.yml; do
+    filename=$(basename "$file")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    output_directory="ESRules/$filename.yaml"
+    sigmac -t elastalert -c winlogbeat.yml --output "$output_directory" "$file"
+done
